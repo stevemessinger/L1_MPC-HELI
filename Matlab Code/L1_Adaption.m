f54a550da51baa2c0c8e0ed1f9e16f4b926b1dd9
matthew fehl <Matthew_Fehl@live.com>
2022-01-12 15:08:23 -0500

    initial commit

---
+++ Matlab Code/L1_Adaption.m
@@ -0,0 +1,4 @@
+%{
+    L1 Adaption element for compensation of matched uncertainties of thrust
+%}
+
--- acado-master/acado-master/interfaces/matlab/examples/ocp/invertedpendulum/draw.m
+++
@@ -1,8 +0,0 @@
-figure;
-hold on;
-
-plot(out.STATES(:,1), out.STATES(:,2), 'r')
-plot(out.STATES(:,1), out.STATES(:,3), 'b')
-hold off;
-
-legend('x', 'alpha');
