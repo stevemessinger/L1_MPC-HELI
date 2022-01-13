3c065976c64079b761fb02d1e73d41f83f983cf9
MatthewFehl365 <48969792+MatthewFehl365@users.noreply.github.com>
2022-01-13 10:09:33 -0500

    FIxed pathing

--- Matlab Code/NMPC/Heli_Control_Main.m
+++ Matlab Code/NMPC/Heli_Control_Main.m
@@ -5,4 +5,4 @@ Processes State data from VICON tracking system, computes the NMPC signal,
 applies L1 adaptive element, and streams control information to hardware
 %}
 
-% setup
+%% Adaption Element Setup
