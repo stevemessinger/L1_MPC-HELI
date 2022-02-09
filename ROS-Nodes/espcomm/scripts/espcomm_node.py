c0e713a899920ce4fee229dc67705cb752d2fc39
MatthewFehl365 <matthew_fehl@live.com>
2022-02-09 10:09:23 -0800

    fixed esp data

--- ROS-Nodes/espcomm/scripts/espcomm_node.py
+++ ROS-Nodes/espcomm/scripts/espcomm_node.py
@@ -22,14 +22,19 @@ def publishBNOData():
 
     while not rospy.is_shutdown():
         message = ws.recv()
-
         data = message.split(':')
+
         msg.ax = float(data[0])
         msg.ay = float(data[1])
         msg.az = float(data[2])
         msg.gx = float(data[3])
         msg.gy = float(data[4])
         msg.gz = float(data[5])
+        msg.sysCal = float(data[6])
+        msg.accelCal = float(data[7])
+        msg.gyroCal = float(data[8])
+        msg.magCal = float(data[9])
+        msg.dt = float(data[10])
 
         pub.publish(msg)
         rate.sleep()
