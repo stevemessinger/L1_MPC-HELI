24c1db05b3dc1f74156c822204ab16bf9ac96066
Steve Messinger <steven.messinger342@gmail.com>
2022-02-09 10:39:32 -0500

    added EKF design

--- matlab_code/EKF/EKF_Design.m
+++ matlab_code/EKF/EKF_Design.m
@@ -8,3 +8,7 @@ Sources: Optimal State Estimation -Dan Simon
 clear all 
 close all 
 clc
+
+load('imuTest.mat') %load imu test data 
+load('mit_imu.mat') %load MIT imu data
+load('mit_vicon.mat') %load MIT imu data
